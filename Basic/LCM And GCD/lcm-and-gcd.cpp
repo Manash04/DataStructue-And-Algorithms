23075948a7ee710628ebdafbb08ed95cf4464c81
Manash Bandhu <125493319+Manash04@users.noreply.github.com>
2024-05-27 13:49:39 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/LCM And GCD/lcm-and-gcd.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<long long> lcmAndGcd(long long A , long long B) {
+        long long  mul = A*B;
+        while(B!=0){
+            
+           long long temp = B;
+            B= A%B;
+            A=temp;
+        }
+        long long gcd=A;
+        
+        long long lcm = mul/A;
+            return {lcm,gcd};
+
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long A,B;
+        
+        cin>>A>>B;
+
+        Solution ob;
+        vector<long long> ans = ob.lcmAndGcd(A,B);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
