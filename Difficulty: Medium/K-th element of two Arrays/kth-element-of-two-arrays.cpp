fbcd1d1e20baf5b7afba37f06551a0a0128d8eeb
Manash Bandhu <125493319+Manash04@users.noreply.github.com>
2024-11-12 22:41:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -7,30 +7,29 @@ using namespace std;
 class Solution {
   public:
     int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
-        int n1= arr1.size();
-        int n2 = arr2.size();
-        int i=0, j=0;
-        int count =0;
-        while(i<n1 && j<n2){
-            if(arr1[i]<arr2[j]){
-                count++;
-                if(count==k) return arr1[i];
-                i++;
-            }else{
-                count++;
-                if(count==k) return arr2[j];
-                j++;
-            }
-        }
-        while(i<n1){
-            count++;
-            if(count==k) return arr1[i];
-                i++;
-        }while(j<n2){
-            count++;
-                if(count==k) return arr2[j];
-                j++;
+        // code here
+        int n1=arr1.size();
+        int n2=arr2.size();
+        if(n1>n2) return kthElement(k , arr2, arr1);
+        int n=n1+n2;
+        int left =k;
+        int low = max(k-n2 , 0), high = min(k,n1);
+        while(low<=high){
+            int mid1=(low+high)/2;
+            int mid2= left-mid1;
+            int l1=INT_MIN, l2=INT_MIN;
+            int r1=INT_MAX , r2=INT_MAX;
+            if(mid1<n1) r1=arr1[mid1];
+            if(mid2<n2) r2=arr2[mid2];
+            if(mid1-1>=0) l1=arr1[mid1-1];
+            if(mid2-1>=0) l2=arr2[mid2-1];
+            if(l1<=r2 && l2<=r1){
+                return max(l1,l2);
+            }else if(l1>r2) high = mid1-1;
+            else low = mid1+1;
+            
         }return 0;
+        
     }
 };
 
